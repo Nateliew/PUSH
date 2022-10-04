7d00b635a3f3eefe9adb45b5667711f9219a8636
root <root@LAPTOP-C6553LAC.localdomain>
2022-10-04 22:33:16 +0800

    first

---
+++ backup/cvtry.js
@@ -0,0 +1,41 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Cv extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.user, { through: "user" });
+      this.hasOne(models.template);
+    }
+  }
+  Cv.init(
+    {
+      // user_id: {
+      //   type: DataTypes.INTEGER,
+      //   references: {
+      //     model: "user",
+      //     key: "id",
+      //   },
+      // },
+      summary: DataTypes.STRING,
+      // template_id: {
+      //   type: DataTypes.INTEGER,
+      //   references: {
+      //     model: "template",
+      //     key: "id",
+      //   },
+      // },
+    },
+    {
+      sequelize,
+      modelName: "cv",
+      underscored: true,
+    }
+  );
+  return Cv;
+};
